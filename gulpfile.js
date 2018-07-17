c25d3bf16aa1004f5772430c163da06e245c325c
Andreas-Just <andreas.wq@gmail.com>
2018-07-17 06:59:32 +0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		svgstore      = require("gulp-svgstore"),
+		imagemin      = require("gulp-imagemin"),
+		webp          = require("gulp-webp"),
+		rsync         = require('gulp-rsync');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/scss/style.scss')
+	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("app/img"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("app/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("app/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("app/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("app/img/webp"));
+});
+
+gulp.task('rsync', function() {
+	return gulp.src('app/**')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Includes files to deploy
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', ['styles', 'js', "sprite", 'images', 'webp', 'browser-sync'], function() {
+	gulp.watch('app/scss/**/*.scss', ['styles']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.html', browserSync.reload)
+});
+
+gulp.task('default', ['watch']);
---
+++ readme.md
@@ -0,0 +1,25 @@
+﻿# Учебный проект «Mishka»
+
+>Студент: Andreas Just. 
+>
+>Наставник: Andreas Just.
+
+
+## Опубликованная версия
+
+Опубликованная версия проекта доступна по адресу:
+
+
+
+> **https://Andreas-Just.github.io/...**
+
+
+## Полезные ссылки
+
+1.  [Использован стартовый шаблон для верстки сайтов на gulp — OptimizedHTML-4](https://webdesign-master.ru/blog/tools/2018-03-02-optimizedhtml-4.html)
+2.  [PSD макет «Mishka» и видеолекции взяты с сайта Соursеhuntеrs.nеt](https://coursehunters.net/course/professionalnyy-html-i-css-uroven-2-potok-15-yanvarya-21-fevralya-2018)
+
+##
+![enter image description here](https://lh3.googleusercontent.com/90mizUVnFFlEye-MOewgvH8z_BzuPoBRFuuwGY21Sdesxtw0uPDpbutukSkIXhzkJwECVT-PaQixDynqVptE-OXDqkovpJahfQea=w600-h102-rw-no)
+
+Репозиторий создан для самообучения по интенсивному онлайн‑курсу [«Продвинутый HTML и CSS»](https://htmlacademy.ru/intensive/adaptive) от [HTML Academy](https://htmlacademy.ru/).
